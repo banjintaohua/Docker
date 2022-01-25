88ed0a6c91a8187f8e83c8d63905f85fe2220c5f
banjintaohua <fjxzyx@gmail.com>
2022-01-25 10:32:42 +0800

    chore(refactor): config

--- Python/build/conf/docker-entrypoint.sh
+++ Python/build/conf/docker-entrypoint.sh
@@ -5,16 +5,16 @@ cd /var/www/app
 
 case "$1" in
   "dev" )
-    exec poetry run uvicorn --host=0.0.0.0 --port 80 app.main:app --reload
+    poetry run uvicorn --host=0.0.0.0 --port 80 app.main:app --reload
     ;;
 
   "debug" )
     # 需要先在 Pycharm 中启用 Debug Server
-    exec poetry run uvicorn --host=0.0.0.0 --port 80 app.main_debug:app --reload
+    poetry run uvicorn --host=0.0.0.0 --port 80 app.main_debug:app --reload
     ;;
 
   "run" )
-    exec poetry run uvicorn --host=0.0.0.0 --port 80 app.main:app
+    poetry run uvicorn --host=0.0.0.0 --port 80 app.main:app
     ;;
 
    * )
