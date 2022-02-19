ff4082b17a38d37efde41a4e61ea8fa01f69e0af
banjintaohua <fjxzyx@gmail.com>
2022-02-19 13:00:47 +0800

    refactor(python): health check

--- Python/build/entrypoint.sh
+++ Python/build/entrypoint.sh
@@ -2,27 +2,40 @@
 
 set -e
 
-if [[  -n ${REMOTE_URL}  ]]; then
+function health_check() {
+  curl -f http://localhost:8080/api/ || exit 1
+
+  return 0
+}
+
+if [[ -n ${REMOTE_URL} ]]; then
   git clone --progress "${REMOTE_URL}" /var/www/app
 fi
 
 cd /var/www/app
 
 case "$1" in
-  "dev" )
-    poetry run uvicorn --host=0.0.0.0 --port 8080 app.main:app --reload
-    ;;
+"dev")
+  health_check
+  poetry run uvicorn --host=0.0.0.0 --port 8080 app.main:app --reload
+  ;;
 
-  "debug" )
-    # 需要先在 Pycharm 中启用 Debug Server
-    poetry run uvicorn --host=0.0.0.0 --port 8080 app.main_debug:app --reload
-    ;;
+"debug")
+  # 需要先在 Pycharm 中启用 Debug Server
+  health_check
+  poetry run uvicorn --host=0.0.0.0 --port 8080 app.main_debug:app --reload
+  ;;
 
-  "run" )
-    poetry run uvicorn --host=0.0.0.0 --port 8080 app.main:app
-    ;;
+"run")
+  poetry run uvicorn --host=0.0.0.0 --port 8080 app.main:app
+  ;;
 
-   * )
-    exec "$@"
-    ;;
+"health")
+  health_check
+  ;;
+
+*)
+  exec "$@"
+  ;;
+
 esac
