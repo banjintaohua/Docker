7953f7da1ed496e9d8f281ce0cd8fc468a2eaebe
banjintaohua <fjxzyx@gmail.com>
2022-03-24 00:09:15 +0800

    refactor(nginx): assign ipam

--- Python/build/entrypoint.sh
+++ Python/build/entrypoint.sh
@@ -22,21 +22,16 @@ cd /var/www/app
 
 case "$1" in
 "dev")
-  poetry run uvicorn --host=0.0.0.0 --port 8080 app.main:app --reload
+  exec poetry run uvicorn --host=0.0.0.0 --port 8080 app.main:app --reload
   ;;
 
 "debug")
   # 需要先在 Pycharm 中启用 Debug Server
-  poetry run uvicorn --host=0.0.0.0 --port 8080 app.main_debug:app --reload
-  health_check
+  exec poetry run uvicorn --host=0.0.0.0 --port 8080 app.main_debug:app --reload
   ;;
 
 "run")
-  poetry run uvicorn --host=0.0.0.0 --port 8080 app.main:app || health_check
-  ;;
-
-"health")
-  health_check
+  exec poetry run uvicorn --host=0.0.0.0 --port 8080 app.main:app || health_check
   ;;
 
 *)
