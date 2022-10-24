bf63049068fa1691f76bc7e807be157a3f573dfb
banjintaohua <fjxzyx@gmail.com>
2022-10-24 11:32:48 +0800

    feat: update ssl

--- Nginx/conf/ssl/update_certificate_example.sh
+++ Nginx/conf/ssl/update_certificate_example.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-containerId=$(docker ps --format "{{.ID}} {{.Names}}" | grep acme | awk '{print $1}')
 domain="*.domain.com"
+if [[ -n $1 ]]; then
+    domain=$1
+fi
+
+containerId=$(docker ps --format "{{.ID}} {{.Names}}" | grep acme | awk '{print $1}')
 
 # 检测证书是否过期
 if docker exec "$containerId" openssl x509 -checkend 604800 -noout -in /acme.sh/"$domain"_ecc/"$domain".cer; then
@@ -13,11 +17,11 @@ echo "Certificate has expired or will do so within 7 days"
 echo "(or is invalid/not found)"
 
 # 更新证书
-docker exec "$containerId" /root/.acme.sh/acme.sh --renew --insecure --force --ecc -d \*.banjintaohua.online
+docker exec "$containerId" /root/.acme.sh/acme.sh --renew --insecure --force --ecc -d "${domain}"
 
 # 删除旧证书
 docker exec "$containerId" mkdir -p "/acme.sh/${domain}_ecc/backup"
-docker exec "$containerId" rm -f /acme.sh/"${domain}"_ecc/backup/wildcard-certificate.crt /acme.sh/\*.banjintaohua.online_ecc/backup/wildcard-certificate.key
+docker exec "$containerId" rm -f /acme.sh/"${domain}"_ecc/backup/wildcard-certificate.crt /acme.sh/"${domain}"_ecc/backup/wildcard-certificate.key
 echo "Old certificate has been deleted"
 docker exec "$containerId" ls -al /acme.sh/"${domain}"_ecc/backup
 
