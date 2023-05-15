fbb90b3d43377e4d6bbccddefcc48055184d93f2
banjintaohua <fjxzyx@gmail.com>
2023-05-15 16:30:51 +0800

    refactor: php entrypoint

--- PHP/build/php-56/entrypoint.sh
+++ PHP/build/php-56/entrypoint.sh
@@ -3,17 +3,19 @@
 set -e
 
 health_check() {
-  # todo
-  return 0
-
+    # todo
+    return 0
 }
 
 case "$1" in
-"health")
-  health_check
-  ;;
+    "health")
+        health_check
+        ;;
+    *)
+        if [ "${1#-}" != "$1" ]; then
+            set -- php-fpm --force-stderr --nodaemonize "$@"
+        fi
 
-*)
-  exec "$@"
-  ;;
+        exec "$@"
+        ;;
 esac
